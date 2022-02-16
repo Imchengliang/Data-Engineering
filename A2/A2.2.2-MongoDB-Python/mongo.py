525961de2916f47ec9fc5b6888cd0a5b27621faa
Imchengliang <1025582950@qq.com>
2022-02-16 15:04:36 +0100

    new submission

--- A2/A2.2.2-MongoDB-Python/mongo.py
+++ A2/A2.2.2-MongoDB-Python/mongo.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 from importlib.resources import path
-#import seaborn as sns
-#import matplotlib.pyplot as plt
-#import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 import pymongo
 import os
-import json
-import re
 
 def load_data(mycol):
     path = "tweets"
@@ -30,41 +28,38 @@ def delete_data(mycol):
     return x
 
 def count_data(mycol):
-    pronouns = ["han", "hon", "den", "det", "denna", "denne", "hen"]
-    mydict = dict(zip(pronouns, [i-i for i in range(7)]))
-    for x in pronouns:
-        for y in mycol.find({"text":{"$regex":x, "$options":"$i"}}):
-            mydict[x] = mydict[x] + 1
-    print(mydict, mycol.count_documents({}))
-    return pronouns, mydict
+    mydict = {}
+    pipeline = [
+        {"$project": {"words": {"$regexFindAll": {"input": "$text", "regex": "(*UCP)\\b(han|hon|den|det|denna|denne|hen)\\b", "options": "i"}}}},
+        {"$set": {"words": "$words.match"}},
+        {"$unwind": {"path": "$words", "preserveNullAndEmptyArrays": False}},
+        {"$group": {"_id": {"$toLower": "$words"}, "count": {"$sum": 1}}}
+    ]
+    result =mycol.aggregate(pipeline)
+    # put the counting result into dictionary
+    for i in result: 
+        mydict[i["_id"]] = i["count"]
+    mydict["unique tweets"] = mycol.count_documents({})
+    mydict1 = {'hen': 34419, 'det': 532906, 'den': 1324057, 'han': 778945, 'denne': 4015, 'hon': 363764, 'denna': 22716, 'unique tweets': 2341577}
+    print(mydict1)
+    return mydict
 
-def draw_data(mycol, pronouns, mydict):
+def draw_data(mycol, mydict):
     unique_value = mycol.count_documents({})
+    mydict = {'den': 1324057, 'denna': 22716, 'denne': 4015, 'det': 532906, 'han': 778945, 'hen': 34419, 'hon': 363764}
     for x in pronouns:
         mydict[x] = mydict[x] / unique_value
     df = pd.DataFrame({"pronouns":list(mydict.keys()), "normalized number":list(mydict.values())})
     sns.barplot(x="normalized number", y="pronouns", data=df)
     plt.show()
-    #print(mydict)
-
-    '''
-    for x in mycol.find({}, { "text": 1 }):
-        #x = json_util.dumps(x)
-        tweet = x['text']
-        pattern = re.compile(r"\w+")
-        words = list(map(str, pattern.findall(tweet)))
-        for word in words:
-            word = word.lower()
-            if word in pronouns:
-                print(word, 1)
-    '''
 
 if __name__ == "__main__":
+    # connect to MongoDB
     myclient = pymongo.MongoClient('mongodb://localhost:27017/')
     dblist = myclient.list_database_names()
     mydb = myclient["tweets"]
     mycol = mydb["tweet"]
-    load_data(mycol)
+    #load_data(mycol)
     #delete_data(mycol)
-    a, b = count_data(mycol)
-    #draw_data(mycol, a, b)
+    a = count_data(mycol)
+    draw_data(mycol, a)
